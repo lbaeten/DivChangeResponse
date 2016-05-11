b2fa395e632064bef89effe46e07ba177ca8f2b7
Lander Baeten <Lander Baeten>
2016-05-11 16:15:02 +0200

    Added stan_extract_cre and _pre functions

--- R/helperfunctions.R
+++ R/helperfunctions.R
@@ -3,8 +3,6 @@
 #========================
 
 # Data input function
-new_duration <- seq(from=min(dta_SR$Duration),to=max(dta_SR$Duration),by=1)
-
 stan_in <- function(infile){
   y <- infile$log_SR_ratio
   n <- length(y)
@@ -12,10 +10,11 @@ stan_in <- function(infile){
   n_study <- max(study)
   
   duration <- infile$Duration
-  
+
+  new_duration <- seq(from=min(infile$Duration),to=max(infile$Duration),by=1)
   n_pred <- length(new_duration)
   
-  list(n=n, n_study=n_study, n_pred, study = study, y=y, duration=duration, new_duration = new_duration)
+  list(n=n, n_study=n_study, n_pred = n_pred, study = study, y=y, duration=duration, new_duration = new_duration)
 }
 
 # Draw samples function
@@ -24,14 +23,27 @@ stan_sampling <- function(object, data, iter = 2000){
            init="random", verbose = F, control = list(adapt_delta = .9, max_treedepth = 10))
 }
 
-# Extract predictions
-stan_extract <- function(object) {
-  stan_pred <- data.frame(expand.grid(new_x = new_duration, new_y = 0))
-  y_quant <- apply(rstan::extract(object)$y_pred, 2, quantile, probs = c(.025, .5, .975)) 
+# Extract credible/prediction intervals
+stan_extract_cre <- function(object, data) {
+  stan_int <- data.frame(expand.grid(new_x = data$new_duration, new_y = 0))
+  y_cre <- apply(rstan::extract(object)$"y_cre", 2, quantile, probs = c(.025, .5, .975)) 
 
-  stan_pred$new_y <- y_quant[2,]
-  stan_pred$plo <- y_quant[1,]
-  stan_pred$phi <- y_quant[3,]
-
-  stan_pred
+  stan_int$new_y <- y_cre[2,]
+  stan_int$plo <- y_cre[1,]
+  stan_int$phi <- y_cre[3,]
+  
+  stan_int
 }
+
+# Extract credible/prediction intervals
+stan_extract_pre <- function(object, data) {
+  stan_int <- data.frame(expand.grid(new_x = data$new_duration, new_y = 0))
+  y_pre <- apply(rstan::extract(object)$"y_pre", 2, quantile, probs = c(.025, .5, .975)) 
+  
+  stan_int$new_y <- y_pre[2,]
+  stan_int$plo <- y_pre[1,]
+  stan_int$phi <- y_pre[3,]
+  
+  stan_int
+}
+
