634e5b05480e42a192ce29b043569e5450b23159
IslaMS <imyerssmith@gmail.com>
2016-05-26 13:08:07 -0700

    Isla has beautified the figures.

--- R/analysis_dor.R
+++ R/analysis_dor.R
@@ -11,18 +11,14 @@ source("R/helperfunctions.R")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-
 ####################################################################
-# here you can switch between the original Dornelas data (dorn_raw) 
-# or the data where study 147 was modified (dorn_modif)
-dataset <- dorn_raw
-#dataset <- dorn_modif
 #
-# choose the response variable and corresponding duration variable
-stan_dta <- stan_in_dorn(dataset, "log_ratio", "duration_lr")
+dorn_rawdataset <- dorn_raw
+dorn_rawstan_dta <- stan_in_dorn(dorn_rawdataset, "log_ratio", "duration_lr")
+dorn_modifdataset <- dorn_modif
+dorn_modifstan_dta <- stan_in_dorn(dorn_modifdataset, "log_ratio", "duration_lr")
 #
 ####################################################################
-
 
 # Construct the Stan models
 #--------------------------
@@ -32,48 +28,124 @@ stan_mod_slope <- stan_model("R/model_duration_slope.stan")
 # Sampling
 #-------------------------
 # linear regression (intercept and slope)
-stan_fit_intslope <- stan_sampling(stan_mod_intslope, stan_dta, iter = 5000)
-print(stan_fit_intslope, pars = c("a","b","sigma"), digits = 4)
+dorn_rawstan_fit_intslope <- stan_sampling(stan_mod_intslope, dorn_rawstan_dta, iter = 5000)
+print(dorn_rawstan_fit_intslope, pars = c("a","b","sigma"), digits = 4)
 
-ggs_density(ggs(stan_fit_intslope, family = "b")) + 
+ggs_density(ggs(dorn_rawstan_fit_intslope, family = "b")) + 
   geom_vline(aes(xintercept = 0)) + 
   ggtitle("Effect size")
 
 # linear regression (only slope)
-stan_fit_slope <- stan_sampling(stan_mod_slope, stan_dta, iter = 5000)
-print(stan_fit_slope, pars = c("b", "sigma"), digits = 4)
+dorn_rawstan_fit_slope <- stan_sampling(stan_mod_slope, dorn_rawstan_dta, iter = 5000)
+print(dorn_rawstan_fit_slope, pars = c("b", "sigma"), digits = 4)
 
+# Sampling
+#-------------------------
+# linear regression (intercept and slope)
+dorn_modifstan_fit_intslope <- stan_sampling(stan_mod_intslope, dorn_modifstan_dta, iter = 5000)
+print(dorn_modifstan_fit_intslope, pars = c("a","b","sigma"), digits = 4)
+
+ggs_density(ggs(dorn_modifstan_fit_intslope, family = "b")) + 
+  geom_vline(aes(xintercept = 0)) + 
+  ggtitle("Effect size")
+
+# linear regression (only slope)
+dorn_modifstan_fit_slope <- stan_sampling(stan_mod_slope, dorn_modifstan_dta, iter = 5000)
+print(dorn_modifstan_fit_slope, pars = c("b", "sigma"), digits = 4)
 
 ####################################################################
-# choose the model for which to draw the graphs
-select_model <- stan_fit_intslope
-#select_model <- stan_fit_slope
+dorn_rawselect_model_intslope <- dorn_rawstan_fit_intslope
+dorn_rawselect_model_slope <- dorn_rawstan_fit_slope
+dorn_modifselect_model_intslope <- dorn_modifstan_fit_intslope
+dorn_modifselect_model_slope <- dorn_modifstan_fit_slope
 ####################################################################
-
 
 # extract credible/prediction intervals
-stan_cre <- stan_extract_cre(select_model, stan_dta)
-stan_pre <- stan_extract_pre(select_model, stan_dta)
+dorn_rawstan_cre_intslope <- stan_extract_cre(dorn_rawselect_model_intslope, dorn_rawstan_dta)
+names(dorn_rawstan_cre_intslope) <- c("duration_lr", "log_ratio", "plo", "phi")
+dorn_rawstan_cre_slope <- stan_extract_cre(dorn_rawselect_model_slope, dorn_rawstan_dta)
+names(dorn_rawstan_cre_slope) <- c("duration_lr", "log_ratio", "plo", "phi")
+
+dorn_rawstan_pre_intslope <- stan_extract_pre(dorn_rawselect_model_intslope, dorn_rawstan_dta)
+names(dorn_rawstan_pre_intslope) <- c("duration_lr", "log_ratio", "plo", "phi")
+dorn_rawstan_pre_slope <- stan_extract_pre(dorn_rawselect_model_slope, dorn_rawstan_dta)
+names(dorn_rawstan_pre_slope) <- c("duration_lr", "log_ratio", "plo", "phi")
+
+dorn_modifstan_cre_intslope <- stan_extract_cre(dorn_modifselect_model_intslope, dorn_modifstan_dta)
+names(dorn_modifstan_cre_intslope) <- c("duration_lr", "log_ratio", "plo", "phi")
+dorn_modifstan_cre_slope <- stan_extract_cre(dorn_modifselect_model_slope, dorn_modifstan_dta)
+names(dorn_modifstan_cre_slope) <- c("duration_lr", "log_ratio", "plo", "phi")
+
+dorn_modifstan_pre_intslope <- stan_extract_pre(dorn_modifselect_model_intslope, dorn_modifstan_dta)
+names(dorn_modifstan_pre_intslope) <- c("duration_lr", "log_ratio", "plo", "phi")
+dorn_modifstan_pre_slope <- stan_extract_pre(dorn_modifselect_model_slope, dorn_modifstan_dta)
+names(dorn_modifstan_pre_slope) <- c("duration_lr", "log_ratio", "plo", "phi")
 
 # graphs
   # //x and y variable need to be changed for log_ratio or slope analysis
-ggplot(dataset, aes(duration_slope, slope)) + 
+dorn_rawstan_cre_intslopePlot <- ggplot(dorn_rawdataset, aes(duration_lr, log_ratio)) + 
   geom_point() +
+  geom_point(aes(x = 90,y = -3.76120012), colour = "red", fill = "white", size = 6, shape = 21) +
+  geom_point(aes(x = 90,y = -3.76120012), colour = "red") +
   geom_hline(aes(yintercept = 0)) +
-  geom_line(data = stan_cre, aes(x = new_x, y = new_y), size = 1.2) +
-  geom_line(data = stan_cre, aes(x = new_x, y = plo), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  geom_line(data = stan_cre, aes(x = new_x, y = phi), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  xlab("\nStudy Duration (years)") +
-  ylab("Effect Size: ln(S2/S1)\n") +
-  theme_bw(base_size=17)
+  geom_line(data = dorn_rawstan_cre_intslope, aes(x = duration_lr, y = log_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.0127, intercept = 0.2936, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = dorn_rawstan_cre_intslope, aes(x = duration_lr, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 100) +
+  ylim(-5, 5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=40, y=4.8, label="a) Original dataset - with intercept", size = 5)
 
-ggplot(dataset, aes(duration_lr, log_ratio)) + 
+dorn_rawstan_cre_slopePlot <- ggplot(dorn_rawdataset, aes(duration_lr, log_ratio)) + 
   geom_point() +
+  geom_point(aes(x = 90,y = -3.76120012), colour = "red", fill = "white", size = 6, shape = 21) +
+  geom_point(aes(x = 90,y = -3.76120012), colour = "red") +
   geom_hline(aes(yintercept = 0)) +
-  geom_line(data = stan_pre, aes(x = new_x, y = new_y), size = 1.2) +
-  geom_line(data = stan_pre, aes(x = new_x, y = plo), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  geom_line(data = stan_pre, aes(x = new_x, y = phi), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  xlab("\nStudy Duration (years)") +
-  ylab("Effect Size: ln(S2/S1)\n") +
-  theme_bw(base_size=17)
+  geom_line(data = stan_cre_slope, aes(x = duration_lr, y = log_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.0036, intercept = 0, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = stan_cre_slope, aes(x = duration_lr, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 100) +
+  ylim(-5, 5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=35, y=4.8, label="b) Original dataset - intercept = 0", size = 5)
+
+dorn_modifstan_cre_intslopePlot <- ggplot(dorn_modifdataset, aes(duration_lr, log_ratio)) + 
+  geom_point() +
+  geom_point(aes(x = 86,y = -2.197224577), colour = "red", fill = "white", size = 6, shape = 21) +
+  geom_point(aes(x = 86,y = -2.197224577), colour = "red") +
+  geom_hline(aes(yintercept = 0)) +
+  geom_line(data = dorn_modifstan_cre_intslope, aes(x = duration_lr, y = log_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.0080, intercept = 0.2197, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = dorn_modifstan_cre_intslope, aes(x = duration_lr, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 100) +
+  ylim(-5, 5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=40, y=4.8, label="c) Modified dataset - with intercept", size = 5)
+
+dorn_modifstan_cre_slopePlot <- ggplot(dorn_modifdataset, aes(duration_lr, log_ratio)) + 
+  geom_point() +
+  geom_point(aes(x = 86,y = -2.197224577), colour = "red", fill = "white", size = 6, shape = 21) +
+  geom_point(aes(x = 86,y = -2.197224577), colour = "red") +
+  geom_hline(aes(yintercept = 0)) +
+  geom_line(data = dorn_modifstan_cre_slope, aes(x = duration_lr, y = log_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.0012, intercept = 0, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = dorn_modifstan_cre_slope, aes(x = duration_lr, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 100) +
+  ylim(-5, 5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=35, y=4.8, label="d) Modified dataset - intercept = 0", size = 5)
+
+library(gridExtra)
+library(grid)
+png(file="dornPlots.png", width=800, height=800)
+grid.arrange(vel_origstan_cre_rintslopePlot, vel_origstan_cre_rslopePlot, vel_updstan_cre_rintslopePlot, vel_updstan_cre_rslopePlot, ncol=2, nrow=2, bottom = textGrob("Study Duration (years)", gp=gpar(fontsize=20)), left = textGrob(expression(paste("Effect size (ln(",S[2]/S[1],"))")), gp=gpar(fontsize=20), rot = 90))
+dev.off()
 
--- R/analysis_vel.R
+++ R/analysis_vel.R
@@ -11,17 +11,14 @@ source("R/helperfunctions.R")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-
 ####################################################################
-# here you can switch between the Vellend 1.0 data (vel_orig) 
-# or Vellend 2.0 data (vel_upd)
-dataset <- vel_orig
-#dataset <- vel_upd
 #
-stan_dta <- stan_in_vel(dataset)
+vel_origdataset <- vel_orig
+vel_origstan_dta <- stan_in_vel(vel_origdataset)
+vel_upddataset <- vel_upd
+vel_updstan_dta <- stan_in_vel(vel_upddataset)
 #
 ####################################################################
-
 
 # Construct the Stan models
 #--------------------------
@@ -31,56 +28,122 @@ stan_mod_rslope <- stan_model("R/model_duration_rslope.stan")
 # Sampling
 #-------------------------
 # Random slopes and intercepts
-stan_fit_rintslope <- stan_sampling(stan_mod_rintslope, stan_dta, iter = 5000)
-print(stan_fit_rintslope, pars = c("mu_a","mu_b", "sigma_a","sigma_b", "sigma"), digits = 4)
+vel_origstan_fit_rintslope <- stan_sampling(stan_mod_rintslope, vel_origstan_dta, iter = 5000)
+print(vel_origstan_fit_rintslope, pars = c("mu_a","mu_b", "sigma_a","sigma_b", "sigma"), digits = 4)
 
-ggs_density(ggs(stan_fit_rintslope, family = "mu_b")) + 
-  geom_vline(aes(xintercept = 0)) + 
-  ggtitle("Effect size")
+ggs_density(ggs(vel_origstan_fit_rintslope, family = "mu_b")) + geom_vline(aes(xintercept = 0)) + ggtitle("Effect size")
 
 # Random slopes and no intercept
-stan_fit_rslope <- stan_sampling(stan_mod_rslope, stan_dta, iter = 5000)
-print(stan_fit_rslope, pars = c("mu_b","sigma_b", "sigma"), digits = 4)
+vel_origstan_fit_rslope <- stan_sampling(stan_mod_rslope, vel_origstan_dta, iter = 5000)
+print(vel_origstan_fit_rslope, pars = c("mu_b","sigma_b", "sigma"), digits = 4)
 
 # optional: compare with precompiled Stan model
-stan_pre <- stan_lmer(log_SR_ratio ~ 1 + Duration + (1 + Duration | Study), data = dataset,
+vel_origstan_pre <- stan_lmer(log_SR_ratio ~ 1 + Duration + (1 + Duration | Study), data = vel_origdataset,
                       chains = 2, iter = 1000)
-print(stan_pre, pars = "Duration", digits = 3)
+print(vel_origstan_pre, pars = "Duration", digits = 3)
 
+# Sampling
+#-------------------------
+# Random slopes and intercepts
+vel_updstan_fit_rintslope <- stan_sampling(stan_mod_rintslope, vel_updstan_dta, iter = 5000)
+print(vel_updstan_fit_rintslope, pars = c("mu_a","mu_b", "sigma_a","sigma_b", "sigma"), digits = 4)
+ggs_density(ggs(vel_updstan_fit_rintslope, family = "mu_b")) + geom_vline(aes(xintercept = 0)) + ggtitle("Effect size")
+
+# Random slopes and no intercept
+vel_updstan_fit_rslope <- stan_sampling(stan_mod_rslope, vel_updstan_dta, iter = 5000)
+print(vel_updstan_fit_rslope, pars = c("mu_b","sigma_b", "sigma"), digits = 4)
+
+# optional: compare with precompiled Stan model
+vel_updstan_pre <- stan_lmer(log_SR_ratio ~ 1 + Duration + (1 + Duration | Study), data = vel_upddataset,
+                      chains = 2, iter = 1000)
+print(vel_updstan_pre, pars = "Duration", digits = 3)
 
 ####################################################################
 # choose the model for which to draw the graphs
-select_model <- stan_fit_rintslope
-#select_model <- stan_fit_rslope
+vel_origselect_model_rintslope <- vel_origstan_fit_rintslope
+vel_origselect_model_rslope <- vel_origstan_fit_rslope
+vel_updselect_model_rintslope <- vel_updstan_fit_rintslope
+vel_updselect_model_rslope <- vel_updstan_fit_rslope
 ####################################################################
 
-
 # extract credible/prediction intervals
-stan_cre <- stan_extract_cre(select_model, stan_dta)
-stan_pre <- stan_extract_pre(select_model, stan_dta)
+vel_origstan_cre_rintslope <- stan_extract_cre(vel_origselect_model_rintslope, stan_dta)
+names(vel_origstan_cre_rintslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+vel_origstan_pre_rintslope <- stan_extract_pre(vel_origselect_model_rintslope, stan_dta)
+names(vel_origstan_pre_rintslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+
+vel_origstan_cre_rslope <- stan_extract_cre(vel_origselect_model_rslope, stan_dta)
+names(vel_origstan_cre_rslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+vel_origstan_pre_rslope <- stan_extract_pre(vel_origselect_model_rslope, stan_dta)
+names(vel_origstan_pre_rslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+
+vel_updstan_cre_rintslope <- stan_extract_cre(vel_updselect_model_rintslope, stan_dta)
+names(vel_updstan_cre_rintslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+vel_updstan_pre_rintslope <- stan_extract_pre(vel_updselect_model_rintslope, stan_dta)
+names(vel_updstan_pre_rintslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+
+vel_updstan_cre_rslope <- stan_extract_cre(vel_updselect_model_rslope, stan_dta)
+names(vel_updstan_cre_rslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
+vel_updstan_pre_rslope <- stan_extract_pre(vel_updselect_model_rslope, stan_dta)
+names(vel_updstan_pre_rslope) <- c("Duration", "log_SR_ratio", "plo", "phi")
 
 # graphs
-ggplot(dataset, aes(Duration, log_SR_ratio)) + 
+vel_origstan_cre_rintslopePlot <- ggplot(vel_origdataset, aes(Duration, log_SR_ratio)) + 
   geom_point() +
   geom_hline(aes(yintercept = 0)) +
-  geom_line(data = stan_cre, aes(x = new_x, y = new_y), size = 1.2) +
-  geom_line(data = stan_cre, aes(x = new_x, y = plo), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  geom_line(data = stan_cre, aes(x = new_x, y = phi), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  xlab("\nStudy Duration (years)") +
-  ylab("Effect Size: ln(S2/S1)\n") +
-  theme_bw(base_size=17) +
-  annotate(geom="text", x=0.25, y=1.35, label="a)") +
-  xlim(c(0,275))
+  geom_line(data = vel_origstan_cre_rintslope, aes(x = Duration, y = log_SR_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.00365, intercept = 0.1118, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = vel_origstan_cre_rintslope, aes(x = Duration, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 300) +
+  ylim(-2.5, 2.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=125, y=2.4, label="a) Original dataset - intercept NOT zero", size = 5)
 
-ggplot(dataset, aes(Duration, log_SR_ratio)) + 
+vel_origstan_cre_rslopePlot <- ggplot(vel_origdataset, aes(Duration, log_SR_ratio)) + 
   geom_point() +
   geom_hline(aes(yintercept = 0)) +
-  geom_line(data = stan_pre, aes(x = new_x, y = new_y), size = 1.2) +
-  geom_line(data = stan_pre, aes(x = new_x, y = plo), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  geom_line(data = stan_pre, aes(x = new_x, y = phi), linetype = 2, size = 1.2, col = "blue", alpha = .5) +
-  xlab("\nStudy Duration (years)") +
-  ylab("Effect Size: ln(S2/S1)\n") +
-  theme_bw(base_size=17) +
-  annotate(geom="text", x=0.25, y=1.35, label="a)") +
-  xlim(c(0,275))
+  geom_line(data = vel_origstan_cre_rslope, aes(x = Duration, y = log_SR_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = 0.00112, intercept = 0, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = vel_origstan_cre_rslope, aes(x = Duration, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 300) +
+  ylim(-2.5, 2.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=105, y=2.4, label="b) Original dataset - intercept = 0", size = 5)
 
+vel_updstan_cre_rintslopePlot <- ggplot(vel_upddataset, aes(Duration, log_SR_ratio)) + 
+  geom_point(colour = "black", fill = "grey", shape = 21) +
+  geom_point(data = vel_origdataset, aes(Duration, log_SR_ratio), colour = "black") +
+  geom_hline(aes(yintercept = 0)) +
+  geom_line(data = vel_updstan_cre_rintslope, aes(x = Duration, y = log_SR_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = -0.00255, intercept = 0.0935, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = vel_updstan_cre_rintslope, aes(x = Duration, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 300) +
+  ylim(-2.5, 2.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=125, y=2.4, label="c) Updated dataset - intercept NOT zero", size = 5)
+
+vel_updstan_cre_rslopePlot <- ggplot(vel_upddataset, aes(Duration, log_SR_ratio)) + 
+  geom_point(colour = "black", fill = "grey", shape = 21) +
+  geom_point(data = vel_origdataset, aes(Duration, log_SR_ratio), colour = "black") +
+  geom_hline(aes(yintercept = 0)) +
+  geom_line(data = vel_updstan_cre_rslope, aes(x = Duration, y = log_SR_ratio), size = 1.2, colour = "blue") +
+  geom_abline(slope = 0.00135, intercept = 0, size = 1.2, linetype = 3, colour = "blue") +
+  geom_ribbon(data = vel_updstan_cre_rslope, aes(x = Duration, ymin = plo, ymax = phi), alpha = 0.3, fill = "blue") +
+  xlim(0, 300) +
+  ylim(-2.5, 2.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme_bw(base_size=15) +
+  annotate(geom="text", x=105, y=2.4, label="d) Updated dataset - intercept = 0", size = 5)
+
+library(gridExtra)
+library(grid)
+png(file="velPlots.png", width=800, height=800)
+grid.arrange(vel_origstan_cre_rintslopePlot, vel_origstan_cre_rslopePlot, vel_updstan_cre_rintslopePlot, vel_updstan_cre_rslopePlot, ncol=2, nrow=2, bottom = textGrob("Study Duration (years)", gp=gpar(fontsize=20)), left = textGrob(expression(paste("Effect size (ln(",S[2]/S[1],"))")), gp=gpar(fontsize=20), rot = 90))
+dev.off()
