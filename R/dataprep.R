84f3a32190d3203bd985e2bd7f61a2e44ced1e3b
Lander Baeten <Lander Baeten>
2016-09-26 20:14:10 +0200

    calculate study indicators with a function add 'impact only' data subsets

--- R/dataprep.R
+++ R/dataprep.R
@@ -23,21 +23,26 @@ vel_upd <- read.csv("data/Vellend_data_updated.csv", header=T) %>%
   tbl_df() %>%
   filter(SR_analysis == 1)
 
+  # "Impact only" data
+vel_orig_I <- vel_orig %>%
+  filter(Driver != "Cessation_grazing", Driver != "Post-disturbance", Driver != "Post-fire")
+
+vel_upd_I<- vel_upd %>%
+  filter(Driver != "Cessation_grazing", Driver != "Post-disturbance", Driver != "Post-fire")
+
   # Calculate study indicators
-n <- nrow(vel_orig)
-vel_orig$Study_nr <- rep(NA,n)
-study_ID <- unique(vel_orig$Study)
-k <- length(study_ID)
-for(j in 1:k){
-  vel_orig$Study_nr[vel_orig$Study == study_ID[j]] <- j
+studyindicators <- function(infile){
+  n <- nrow(infile)
+  Study_nr <- rep(NA, n)
+  study_ID <- unique(infile$Study)
+  k <- length(study_ID)
+  for(j in 1:k){
+    Study_nr[infile$Study == study_ID[j]] <- j
+  }
+  return(Study_nr)
 }
 
-n <- nrow(vel_upd)
-vel_upd$Study_nr <- rep(NA,n)
-study_ID <- unique(vel_upd$Study)
-k <- length(study_ID)
-for(j in 1:k){
-  vel_upd$Study_nr[vel_upd$Study == study_ID[j]] <- j
-}
-
-rm(list = c("j", "k", "n", "study_ID"))
+vel_orig$Study_nr <- studyindicators(vel_orig)
+vel_orig_I$Study_nr <- studyindicators(vel_orig_I)
+vel_upd$Study_nr <- studyindicators(vel_upd)
+vel_upd_I$Study_nr <- studyindicators(vel_upd_I)
